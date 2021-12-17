cca535684a7c2207784657fd7bd99d3a07c2e57d
doezii <96138185+doezii@users.noreply.github.com>
2021-12-17 15:19:46 +0300

    Make not valid test

--- test_nums.py
+++ test_nums.py
@@ -1,7 +1,7 @@
 import nums
 
 def test_program():
-    assert nums.calculate_max([0, -5, 14, 4, 0]) == 14, "Not valid test [calculate_max]"
+    assert nums.calculate_max([0, -5, 14, 4, 0]) == 15, "Not valid test [calculate_max]"
     assert nums.calculate_min([0, 0, -1, 10, -3, 6]) == -3, "Not valid test [calculate_min]"
     assert nums.calculate_sum([9, 3, 3]) == 15, "Not valid test [calculate_sum]"
     assert nums.calculate_mult([5, 3, -1, 2]) == -30, "Not valid test [get_max]"
